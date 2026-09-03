dfb4b73fe86efab8ba265a9c958654fac7734226
Ali Lee <fcqz121314@gmail.com>
2026-09-03 05:15:59 -0400

    ci: remove system Chromium override

--- test/playwright.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@playwright/test';
-
-const executablePath = process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH;
-
-export default defineConfig({
-  use: {
-    launchOptions: executablePath ? { executablePath } : {},
-  },
-});
